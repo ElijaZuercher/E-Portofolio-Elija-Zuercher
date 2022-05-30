55061cdec75e8f0ad3695031a812c6b0f0debc4a
ElijaZuercher <cuzimunderlevel@gmail.com>
2022-05-30 23:41:48 +0200

    final commit of the day rahmen bild von mir

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict"
+let githubicon = document.getElementById("github")
+
+githubicon.addEventListener('click', e =>{
+    window.open("https://github.com/ElijaZuercher", "newwindow", "status=1,fullscreen=1")
+})     
+
